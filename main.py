31de5b2fc9bdfc76a98c2ed368ccbe87aca6a51d
lusxvr <luis.wiedmann@gmail.com>
2025-06-25 16:02:19 +0000

    batched inputs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
 import torch
-from PIL import Image
 from torchvision import transforms
+from datasets import load_dataset
+
+dataset = load_dataset("Lin-Chen/MMStar")
+model = torch.jit.load("models/sscd_disc_mixup.torchscript.pt")
 
 normalize = transforms.Normalize(
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],
@@ -17,11 +19,20 @@ skew_320 = transforms.Compose([
     normalize,
 ])
 
-model = torch.jit.load("models/sscd_disc_mixup.torchscript.pt")
-img = Image.open("data/image_mmstar.jpg").convert('RGB')
+image = dataset['val'][0]['image'].convert('RGB')
 
-batch1 = small_288(img).unsqueeze(0)
-batch2 = skew_320(img).unsqueeze(0)
+small_image = small_288(image)
+square_image = skew_320(image)
+
+batch = torch.stack([small_image, small_image])
+
+embeddings = model(batch)
+print(embeddings.shape)
+print("Similarity:", embeddings[0].dot(embeddings[1]).item())
+print("Distance:", (embeddings[0]-embeddings[1]).norm().item())
+
+batch1 = small_288(image).unsqueeze(0)
+batch2 = skew_320(image).unsqueeze(0)
 
 embedding1 = model(batch1)[0, :]
 embedding2 = model(batch2)[0, :]
@@ -29,3 +40,4 @@ embedding2 = model(batch2)[0, :]
 print(embedding1.shape, embedding2.shape)
 print("Similarity:", embedding1.dot(embedding2).item())
 print("Distance:", (embedding1-embedding2).norm().item())
+
